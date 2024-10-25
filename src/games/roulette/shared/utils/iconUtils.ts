24eda679493bd8f968bf334a0522009635bcd0c1
nikita-chabaniuk <nikita.chabaniuk@hover.to>
2024-10-25 10:08:48 +0300

    path

--- src/games/roulette/shared/utils/iconUtils.ts
+++ src/games/roulette/shared/utils/iconUtils.ts
@@ -1,9 +1,3 @@
-import bet50 from '../../../../assets/roulette/bet-50.png';
-import bet100 from '../../../..assets/roulette/bet-100.png';
-import bet200 from '../../../..assets/roulette/bet-200.png';
-import bet400 from '../../../..assets/roulette/bet-400.png';
-import bet800 from '../../../..assets/roulette/bet-800.png';
-
 export const getIcon = (currentBet: number) => {
   const isMoreThan50 = currentBet >= 50;
   const isMoreThan100 = currentBet >= 100;
@@ -11,10 +5,10 @@ export const getIcon = (currentBet: number) => {
   const isMoreThan400 = currentBet >= 400;
   const isMoreThan800 = currentBet >= 800;
 
-  if (isMoreThan800) return bet800;
-  if (isMoreThan400) return bet400;
-  if (isMoreThan200) return bet200;
-  if (isMoreThan100) return bet100;
-  if (isMoreThan50) return bet50;
+  if (isMoreThan800) return '/assets/roulette/bet-800.png';
+  if (isMoreThan400) return '/assets/roulette/bet-400.png';
+  if (isMoreThan200) return '/assets/roulette/bet-200.png';
+  if (isMoreThan100) return '/assets/roulette/bet-100.png';
+  if (isMoreThan50) return '/assets/roulette/bet-50.png';
   return null;
 };
